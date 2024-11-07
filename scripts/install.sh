6bdbc7a99213798e2480593e823bd0faa91e61e3
Robert Todea <rtodea@mobileinsight.com>
2024-11-07 17:13:22 +0200

    Add installation steps for google-test and benchmark
    This commit updates the `install.sh` script to include steps for installing `google-test` and `benchmark` system-wide. These additions will ensure that the necessary libraries are available for subsequent steps.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,10 @@
 
 cd roll-dice/libraries
 
+# install `google-test` system wide ?
+
+# install `benchmark` system wide ?
+
 # install system-wide `oatpp` libary
 git clone https://github.com/oatpp/oatpp.git
 cd oatpp
