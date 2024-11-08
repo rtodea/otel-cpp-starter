c8b547d7e30a8e95f15cc696bc359db07431ea8d
Robert Todea <rtodea@mobileinsight.com>
2024-11-08 08:40:20 +0200

    Separate install scripts for Oatpp and OpenTelemetry
    This commit removes the monolithic `install.sh` script and introduces two separate scripts: `install-oatpp-library.sh` and `install-opentelemetry-cpp.sh`. The respective Dockerfile has been updated to reflect these changes, ensuring a more modular and maintainable installation process.

--- scripts/install-oatpp-library.sh
+++ scripts/install-oatpp-library.sh
@@ -14,12 +14,3 @@ mkdir build
 cd build
 cmake ..
 make
-
-# install open-telemetry
-git clone https://github.com/open-telemetry/opentelemetry-cpp.git
-cd opentelemetry-cpp
-mkdir build
-cd build
-cmake -DBUILD_TESTING=OFF ..
-cmake --build .
-cmake --install . --prefix ../../otel-cpp
---
+++ scripts/install-opentelemetry-cpp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install open-telemetry
+git clone https://github.com/open-telemetry/opentelemetry-cpp.git
+cd opentelemetry-cpp
+mkdir build
+cd build
+cmake -DBUILD_TESTING=OFF ..
+cmake --build .
+cmake --install . --prefix ../../otel-cpp
