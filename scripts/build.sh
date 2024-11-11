b00f1a3de681a886aea91c9b7a469b3065f1234a
Robert Todea <rtodea@mobileinsight.com>
2024-11-11 18:44:44 +0200

    WIP

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+cd roll-dice
+
+rm -rf build
 mkdir build
 cd build
+
 cmake ..
 cmake --build .
+
---
+++ scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+LD_LIBRARY_PATH=/app/roll-dice/libraries/otel-cpp/lib/ ./dice-server
